0d4b6ef920533f8350013bcec275dfacfbef5eb2
Mateusz Kubicki <drevan657@gmail.com>
2019-01-13 00:50:06 +0100

    scaling works

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,6 +5,8 @@ import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
@@ -17,11 +19,14 @@ public class Main extends Application {
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("sample.fxml"));
             Parent root = fxmlLoader.load();
             Controller controller = fxmlLoader.getController();
+            //AnchorPane anchor = (AnchorPane) root.getChildrenUnmodifiable().get(0);
             WebView webView = (WebView) root.getChildrenUnmodifiable().get(0);
+
             GameEngine engine = new GameEngine(webView,"pl");
             controller.init(engine);
             engine.addObserver(controller);
             stage.setScene(new Scene(root));
+            stage.setMaximized(true);
             stage.show();
             engine.newGame();
 
